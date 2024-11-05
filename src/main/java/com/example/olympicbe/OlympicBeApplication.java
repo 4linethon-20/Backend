052e287ddad09edba507edc4cc7d1a80c0b428b8
sejin <son7187@naver.com>
2024-11-06 03:12:53 +0900

    first commit

---
+++ src/main/java/com/example/olympicbe/OlympicBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.olympicbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlympicBeApplication {
+
+   public static void main(String[] args) {
+      SpringApplication.run(OlympicBeApplication.class, args);
+   }
+
+}
---
+++ src/test/java/com/example/olympicbe/OlympicBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.olympicbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlympicBeApplicationTests {
+
+   @Test
+   void contextLoads() {
+   }
+
+}
